a3365cacb57d49c929a3a4c0c07d67be1b438b26
anav5704 <anavkki@gmail.com>
2024-04-26 22:41:21 +1200

    style: redesign clients section

--- libs/shared/ui/src/lib/server/clients.tsx
+++ libs/shared/ui/src/lib/server/clients.tsx
@@ -1,23 +1,19 @@
+import { Container } from "@previous.js/shared-ui/server"
 import Image from "next/image"
-import AlienWare from "../../assets/alienware.svg"
-import Astro from "../../assets/astro.svg"
-import Clerk from "../../assets/clerk.svg"
-import Epic from "../../assets/epic.svg"
-import Firefox from "../../assets/firefox.svg"
-import Next from "../../assets/next.svg"
-import Prisma from "../../assets/prisma.svg"
+import IGN from "../../assets/ign.svg"
+import Notion from "../../assets/notion.svg"
+import OpenAI from "../../assets/openai.svg"
 import Spotify from "../../assets/spotify.svg"
-import Supabase from "../../assets/supabase.svg"
-import Svelte from "../../assets/svelte.svg"
-import Upstash from "../../assets/upstash.svg"
-import Xbox from "../../assets/xbox.svg"
+import Target from "../../assets/target.svg"
+import Twitch from "../../assets/twitch.svg"
+import Vodafone from "../../assets/vodafone.svg"
 
-const logos = [Astro, Next, Clerk, Epic, Svelte, Firefox, AlienWare, Supabase, Spotify, Upstash, Xbox, Prisma]
+const logos = [Vodafone, OpenAI, Twitch, IGN, Notion, Spotify, Target]
 
 export const Clients = () => {
     return (
-        <div className="relative fade w-full inline-flex flex-nowrap overflow-hidden">
-            <ul className="flex items-center justify-center md:justify-start [&_li]:mx-16 [&_img]:max-w-none animate-infinite-scroll-left">
+        <ul className="relative fade w-full">
+            <Container className=" flex items-center justify-between">
                 {logos.map((logo, index) => (
                     <li key={`first-logo-${index}`} className="col-span-1">
                         <Image
@@ -29,20 +25,7 @@ export const Clients = () => {
                         />
                     </li>
                 ))}
-            </ul >
-            <ul className="flex items-center justify-center md:justify-start [&_li]:mx-16 [&_img]:max-w-none animate-infinite-scroll-left">
-                {logos.map((logo, index) => (
-                    <li key={`second-logo-${index}`} className="col-span-1">
-                        <Image
-                            src={logo}
-                            alt="Logo"
-                            height={50}
-                            width={50}
-                            quality={100}
-                        />
-                    </li>
-                ))}
-            </ul >
-        </div >
+            </Container>
+        </ul >
     )
 }
